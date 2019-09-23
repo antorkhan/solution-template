93fc9c3355ed377279f693ae9b917c7d932159a8
MD Tawab Alam Khan <cs.antorkhan@gmail.com>
2019-09-24 02:07:59 +0600

    File IO

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -1,6 +1,10 @@
 package com.tigerit.exam;
 
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import static com.tigerit.exam.IO.*;
 
 /**
@@ -10,19 +14,35 @@ import static com.tigerit.exam.IO.*;
  * application's execution points start from inside run method.
  */
 public class Solution implements Runnable {
+    static private final String INPUT = "C:\\sites\\solution-template\\src\\main\\java\\com\\tigerit\\exam\\input.txt";
+    static private final String OUTPUT = "C:\\sites\\solution-template\\src\\main\\java\\com\\tigerit\\exam\\output.txt";
     @Override
     public void run() {
+        FileInputStream instream = null;
+        PrintStream outstream = null;
+
+        try {
+            instream = new FileInputStream(INPUT);
+            outstream = new PrintStream(new FileOutputStream(OUTPUT));
+            System.setIn(instream);
+            System.setOut(outstream);
+        } catch (Exception e) {
+            System.err.println("Error Occurred.");
+        }
+
         // your application entry point
 
         // sample input process
         int T = readLineAsInteger();
-
         for (int i = 0; i < T; i++) {
             int nT = readLineAsInteger();
             for (int j = 0; j < nT; j++) {
                 String table_name = readLine();
-                int num_of_row = readLineAsInteger();
-                int num_col_col = readLineAsInteger();
+                String row_and_col_raw = readLine();
+                String row_and_col[] = row_and_col_raw.split(" ");
+
+                int num_of_row = Integer.parseInt(row_and_col[0]);
+                int num_col_col = Integer.parseInt(row_and_col[1]);
                 String table_names = readLine();
 //                  Column name input
                 for (int k = 0; k < num_of_row; k++) {
@@ -37,8 +57,8 @@ public class Solution implements Runnable {
                 System.out.println("Test: 1");
             }
         }
-
-        // sample output process
+//
+//         sample output process
 //        printLine(string);
 //        printLine(integer);
     }
